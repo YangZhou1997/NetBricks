915c6f3eb6b3895caca0b0865350386f057eec85
yangz <yangz@rumi.harvard.edu>
2019-07-23 10:29:19 -0400

    add more home option to config

--- examples/config.sh
+++ examples/config.sh
@@ -3,7 +3,8 @@
 # for both real and local run
 #HOME=/home/vagrant
 # HOME=/users/yangzhou
-HOME=/opt
+# HOME=/opt
+HOME=/home/yangz
 TRAFFIC=$HOME/traffic/ictf2010/merged.pcap
 
 export LD_LIBRARY_PATH="$HOME/NetBricks/native:/opt/dpdk/dpdk-stable-17.08/build/lib:"

82df70de4709b395215b701a4e9da09e6d017977
yangzhou <yangzhou@g.harvard.edu>
2019-07-29 17:26:15 -0400

    add binary search to limitmem.py

--- examples/limitmem.py
+++ examples/limitmem.py
@@ -9,8 +9,9 @@ from threading import Event, Thread
 # cgroup seems including dpdk memory usage
 # DPDK_MEM = (5 * 2 * 1024 * 1024)
 
+# seems you cannot pass 1.2M to cgroup. 
 CmdLimitMemBg = {
-	'start': './limitmem {mem}M bash run_real.sh {task} {num_queue} 2>/dev/null &',
+	'start': './limitmem {mem}K bash run_real.sh {task} {num_queue} 2>/dev/null &',
 	'kill': 'sudo pkill limitmem && sudo pkill head && sudo pkill {task}'
 }
 
@@ -69,6 +70,8 @@ def cgroup_polling(cgroup_name):
 		max_mem_usage = int(max_memusage_results.rstrip("\n").split()[1])
 
 def run_limitmem(task, pktgen, memsize):
+	print colored("run_limitmem: task" + " " + pktgen + " " + str(memsize) + "KB", 'yellow')
+
 	global stop_mark
 	global mem_usages
 	global max_mem_usage
@@ -121,6 +124,8 @@ def run_limitmem(task, pktgen, memsize):
 	stop_mark = True
 	polling.join()
 	os.system(CmdLimitMemBg['kill'].format(task = task))
+	time.sleep(5) # wait for the port being restored.
+
 
 	return 0
 
@@ -134,17 +139,36 @@ if __name__ == '__main__':
 
 	for task in tasks:
 		for pktgen_type in pktgen_types:
-			res = run_limitmem(task, pktgen_type, 7.5)
+			low_mem = 1 #KB
+			high_mem = 4 * 1024 * 1024 # KB
+			min_mem = 4 #KB
+			while 1 and (high_mem - low_mem) > min_mem:
+				cur_mem_limit = (low_mem + high_mem) / 2
+				res = run_limitmem(task, pktgen_type, int(cur_mem_limit))
+				if res == -1:
+					low_mem = cur_mem_limit
+				else:
+					high_mem = cur_mem_limit
+
+			print low_mem, cur_mem_limit, high_mem
+			print "using %lf to retest" % (high_mem)
+			res = run_limitmem(task, pktgen_type, high_mem)
+
+			if res == -1:
+				print "retesting fails"
+			else:
+				print "retesting succeeds"
 
 			total_mem_usages = map(lambda x: x / (1024 * 1024.0), mem_usages)
 			max_total_mem_usages = max_mem_usage  / (1024 * 1024.0)		
 			
 			print total_mem_usages
-			print colored("peak_total_mem_usage: " + str(max_total_mem_usages), 'green')
+			print colored("[Cgroup direct]: peak_total_mem_usage: " + str(max_total_mem_usages), 'green')
+			print colored("[Binary search]: peak_total_mem_usage: " + str(high_mem / 1024.0), 'green')
 
 			limitmem_res.write(task + "," + pktgen_type + "\n")
 			limitmem_res.write(str(total_mem_usages) + "\n")
-			limitmem_res.write(str(max_total_mem_usages) + "\n")
+			limitmem_res.write(str(max_total_mem_usages) + "," + str(high_mem / 1024.0) + "\n")
 			limitmem_res.flush()
 	
 	limitmem_res.close()

33bd79b8171a7effdd4853a78495e650028c16e5
yangzhou <yangzhou@g.harvard.edu>
2019-07-31 01:01:05 -0400

    constrain acl-fw hash table size

--- examples/measure_throughput.py
+++ examples/measure_throughput.py
@@ -22,6 +22,54 @@ def kill_keyword(task):
 	else:
 		return task
 
+def task_exec_reboot(task, pktgen_types, num_queue, repeat_num, throughput_res):
+	# repeat the booting until succeeding
+	# sometimes, failed dpdk application can reclaim hugepage.
+	for i in range(repeat_num):
+		for pktgen_type in pktgen_types:	
+			fail_count_inner = 0
+			while(1):
+				print "start task %s" % (task,)
+				os.system(CmdNetBricks['start'].format(task=task, num_queue=num_queue))
+				time.sleep(5) # wait for task gets actually started
+			
+				print "start pktgen %s" % (pktgen_type,)
+				pktgen_results = os.popen(CmdPktgen['start'].format(type=pktgen_type)).read()
+				print "end pktgen %s" % (pktgen_type,)
+
+				print pktgen_results
+				start_index = pktgen_results.find(start_string) + len(start_string) 
+				# this task executes error. 
+				if start_index == -1:
+					print colored("%s %s %s fails" % (task, pktgen_type, num_queue), 'red')
+					fail_count_inner += 1
+					os.system(CmdNetBricks['kill'].format(task=kill_keyword(task)))
+					time.sleep(5) # wait for the port being restored.
+					continue
+				end_index = pktgen_results.find(end_string, start_index)
+				if end_index == -1:
+					print colored("%s %s %s fails" % (task, pktgen_type, num_queue), 'red')
+					os.system(CmdNetBricks['kill'].format(task=kill_keyword(task)))
+					time.sleep(5) # wait for the port being restored.
+					fail_count_inner += 1
+					continue
+				
+				if fail_count_inner > 5:
+					return -1
+				
+				throughput_val = pktgen_results[start_index: end_index]
+				throughput_val = float(throughput_val)
+
+				print colored("throughput_val: %lf" % (throughput_val,), 'blue')
+				throughput_res.write(task + "," + pktgen_type + "," + str(num_queue) + "," + str(throughput_val) + "\n")
+				throughput_res.flush()
+
+				os.system(CmdNetBricks['kill'].format(task=kill_keyword(task)))
+				time.sleep(5) # wait for the port being restored.
+
+	return 0
+
+
 def task_exec(task, pktgen_types, num_queue, repeat_num, throughput_res):
 	# repeat the booting until succeeding
 	fail_count_inner = 0
@@ -80,9 +128,11 @@ def task_exec(task, pktgen_types, num_queue, repeat_num, throughput_res):
 
 	return 0
 
-tasks = ["acl-fw", "dpi", "lpm", "maglev", "monitoring", "nat-tcp-v4"]
+tasks_nonreboot = ["dpi", "lpm", "maglev"]
+tasks_reboot = ["acl-fw", "monitoring", "nat-tcp-v4"]
 pktgens = ["ICTF", "CAIDA64", "CAIDA256", "CAIDA512", "CAIDA1024"]
-tasks_ipsec = ["acl-fw-ipsec", "dpi-ipsec", "lpm-ipsec", "maglev-ipsec", "monitoring-ipsec", "nat-tcp-v4-ipsec"]
+tasks_ipsec_nonreboot = ["dpi-ipsec", "lpm-ipsec", "maglev-ipsec"]
+tasks_ipsec_reboot = ["acl-fw-ipsec", "monitoring-ipsec", "nat-tcp-v4-ipsec"]
 pktgens_ipsec = ["ICTF_IPSEC", "CAIDA64_IPSEC", "CAIDA256_IPSEC", "CAIDA512_IPSEC", "CAIDA1024_IPSEC"]
 
 num_queues = [1, 2, 3, 4, 5, 6]
@@ -98,19 +148,34 @@ if __name__ == '__main__':
 	run_count = 0
 	fail_count = 0
 
-	# for task in tasks:
-	# 	for num_queue in num_queues:
-	# 		run_count += 1
-	# 		status = task_exec(task, pktgens, num_queue, 10, throughput_res)
-	# 		if status == -1:
-	# 			fail_count += 1
-	# 			fail_cases.append(task + " " + num_queue)
+	for task in tasks_nonreboot:
+		for num_queue in num_queues:
+			run_count += 1
+			status = task_exec(task, pktgens, num_queue, 10, throughput_res)
+			if status == -1:
+				fail_count += 1
+				fail_cases.append(task + " " + num_queue)
 
-	
-	for task in tasks_ipsec:
+	for task in tasks_reboot:
+		for num_queue in num_queues:
+			run_count += 1
+			status = task_exec_reboot(task, pktgens, num_queue, 10, throughput_res)
+			if status == -1:
+				fail_count += 1
+				fail_cases.append(task + " " + num_queue)
+
+	for task in tasks_ipsec_nonreboot:
 		for num_queue in num_queues:
 			run_count += 1
 			status = task_exec(task, pktgens_ipsec, num_queue, 10, throughput_res)
+			if status == -1:
+				fail_count += 1
+				fail_cases.append(task + " " + num_queue)
+
+	for task in tasks_ipsec_reboot:
+		for num_queue in num_queues:
+			run_count += 1
+			status = task_exec_reboot(task, pktgens_ipsec, num_queue, 10, throughput_res)
 			if status == -1:
 				fail_count += 1
 				fail_cases.append(task + " " + num_queue)

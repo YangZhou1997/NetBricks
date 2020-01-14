1cc232f76dacd289ce6aff27ca61d28f9c3bd1a9
Yang Zhou <yangzhou@g.harvard.edu>
2020-01-14 11:34:31 -0500

    new br rumi

--- examples/config.sh
+++ examples/config.sh
@@ -14,7 +14,7 @@ export LD_LIBRARY_PATH="$HOME/NetBricks/native:/opt/dpdk/dpdk-stable-17.08/build
 export RUST_BACKTRACE=1
 
 # for real run
-PORT=0000:02:00.0
+PORT=0000:04:00.0
 CORE=0
 POOL_SIZE=512
 MODE=release
